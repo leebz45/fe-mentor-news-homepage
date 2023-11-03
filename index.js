c1192b49388a1108a49356a2096d5c7f7fbdafc0
leebz45 <43051856+leebz45@users.noreply.github.com>
2023-11-03 18:42:06 -0400

    Commit project to repo

---
+++ index.js
@@ -0,0 +1,28 @@
+const menuButton = document.getElementById('nav-menu-btn');
+const menuCloseButton = document.getElementById('nav-menu-close-btn');
+
+// Add event listener to open the mobile menu
+menuButton.addEventListener('click', (e) => {
+  const menu = document.getElementById('mobile-menu');
+  const menuDrawer = document.getElementById('mobile-menu-drawer');
+
+  document.body.style.overflow = 'hidden';
+  menu.ariaHidden = false;
+
+  // Delay adding this class so the animation will work
+  setTimeout(() => {
+    menuDrawer.classList.toggle('mobile-drawer-open');
+  }, 50);
+});
+
+// Add event listener to close the mobile menu
+menuCloseButton.addEventListener('click', (e) => {
+  const menu = document.getElementById('mobile-menu');
+  const menuDrawer = document.getElementById('mobile-menu-drawer');
+  menuDrawer.classList.toggle('mobile-drawer-open');
+
+  setTimeout(() => {
+    menu.ariaHidden = true;
+    document.body.style.overflow = 'visible';
+  }, 500);
+});
